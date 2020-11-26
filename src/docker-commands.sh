c50f4d220cab2506acfdad82276e2f49249c0ace
Tyrone Scafe <tscafejr@gmail.com>
2020-11-26 00:53:36 -0500

    update start and stop mongo server commands

--- src/docker-commands.sh
+++ src/docker-commands.sh
@@ -1,19 +1,25 @@
 #!/usr/bin/env bash
 
-DATA_DIRECTORY="mongodata";
+DATA_DIRECTORY_PATH="$HOME/mongodata";
 
 pullDockerImage(){
   docker pull mongo;
 }
 
 createDataDirectory(){
-  if [ ! -d "/$DATA_DIRECTORY" ]; then
-      mkdir -p "/$DATA_DIRECTORY";
+  if [ ! -d "$DATA_DIRECTORY_PATH" ]; then
+      mkdir -p "$DATA_DIRECTORY_PATH";
   fi
 }
 
 runMongoImage(){
-  docker run -it -v "$DATA_DIRECTORY":/data/db --name mongodb -d mongo;
+  # shellcheck disable=SC2143
+  if [ "$(docker ps -a | grep mongodb)" ];
+   then
+     docker restart mongodb
+   else
+     docker run -it -v "$DATA_DIRECTORY_PATH":/data/db -p 27017:27017 --name mongodb -d mongo;
+  fi
 }
 
 stopMongoServer(){
